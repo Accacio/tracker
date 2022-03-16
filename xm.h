e725ea4a1032f14866b7763eb7d1ad91db166eb6
Accacio <raccacio@poli.ufrj.br>
2022-03-17 00:34:25 +0100

    function to save file

--- xm.h
+++ xm.h
@@ -462,6 +462,121 @@ read_song_from_file (const char *filename, XM_song *song)
 }
 
 void
+save_song_to_file (const char *output_filename, XM_song *song)
+{
+  /* NOTE(accacio): maybe it would be better to save to a buffer and then use
+   * only one fwrite */
+  FILE *xm_file = fopen (output_filename, "wb");
+  fwrite (&song->header, sizeof (XM_song_header), 1, xm_file);
+  for (int pat_idx = 0; pat_idx < song->header.n_patterns; pat_idx++)
+    {
+      XM_pattern *pattern = &song->patterns[pat_idx];
+
+      uint32_t max_pattern_size
+          = 5 * pattern->n_channels * pattern->header.n_rows;
+      uint8_t *compact_pattern = (uint8_t *) malloc (max_pattern_size);
+      uint8_t *pointer = compact_pattern;
+
+      for (int j = 0; j < pattern->header.n_rows; j++)
+        for (int i = 0; i < pattern->n_channels; i++)
+          {
+            XM_pattern_note note = pattern->data[i][j];
+
+            uint8_t mask = 0x80;
+            mask |= note.note ? 0x01 : 0;
+            mask |= note.instrument ? 0x02 : 0;
+            mask |= note.volume ? 0x04 : 0;
+            mask |= note.effect ? 0x08 : 0;
+            mask |= note.effect_parameter ? 0x10 : 0;
+
+            if (mask & 0x0F)
+              {
+                *pointer++ = note.note;
+                *pointer++ = note.instrument;
+                *pointer++ = note.volume;
+                *pointer++ = note.effect;
+                *pointer++ = note.effect_parameter;
+              }
+            else
+              {
+                *pointer++ = mask;
+
+                if (mask & 0x01)
+                  *pointer++ = note.note;
+                if (mask & 0x02)
+                  *pointer++ = note.instrument;
+                if (mask & 0x04)
+                  *pointer++ = note.volume;
+                if (mask & 0x08)
+                  *pointer++ = note.effect;
+                if (mask & 0x10)
+                  *pointer++ = note.effect_parameter;
+              }
+          }
+      pattern->header.length = (uint16_t) (pointer - compact_pattern);
+
+      fwrite (&pattern->header, sizeof (XM_pattern_header), 1, xm_file);
+
+      fwrite (compact_pattern, pattern->header.length, 1, xm_file);
+      free (compact_pattern);
+    }
+
+  for (int inst_idx = 0; inst_idx < song->header.n_instruments; inst_idx++)
+    {
+      XM_instrument *instrument = &song->instruments[inst_idx];
+
+      fwrite (&instrument->header, sizeof (XM_instrument_header), 1, xm_file);
+
+      if (instrument->header.n_samples > 0)
+        {
+          fwrite (&instrument->extended_header,
+                  sizeof (XM_extended_instrument_header), 1, xm_file);
+
+          for (int sample_idx = 0; sample_idx < instrument->header.n_samples;
+               sample_idx++)
+            {
+              XM_sample *sample = &instrument->samples[sample_idx];
+
+              fwrite (&sample->header, sizeof (XM_sample_header), 1, xm_file);
+            }
+
+          for (int sample_idx = 0; sample_idx < instrument->header.n_samples;
+               sample_idx++)
+            {
+              XM_sample *sample = &instrument->samples[sample_idx];
+
+              if ((sample->header.type & 0X10)) /* if 16bit */
+                {
+                  int16_t old = sample->data[0];
+                  fwrite (&old, sizeof (int16_t), 1, xm_file);
+                  for (int i = 1; i < sample->n_samples; i++)
+                    {
+                      old = sample->data[i] - sample->data[i - 1];
+                      fwrite (&old, sizeof (int16_t), 1, xm_file);
+                    }
+                }
+              else
+                {
+                  int8_t old = sample->data[0];
+                  fwrite (&old, sizeof (int8_t), 1, xm_file);
+                  for (int i = 1; i < sample->n_samples; i++)
+                    {
+                      old = sample->data[i] - sample->data[i - 1];
+                      fwrite (&old, sizeof (int8_t), 1, xm_file);
+                    }
+                }
+            }
+        }
+      else
+        {
+          int32_t sample_header_size = sizeof (XM_sample_header);
+          fwrite (&sample_header_size, sizeof (int32_t), 1, xm_file);
+        }
+    }
+  fclose (xm_file);
+}
+
+void
 print_song (XM_song song)
 {
   printf ("=== SONG ===\n");
